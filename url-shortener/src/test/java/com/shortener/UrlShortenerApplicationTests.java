1516102511edb741c9c4fbee94e564afe39369d1
Pratulya <pratulyapratap@gmail.com>
2026-04-19 13:05:13 +0530

    feat(phase-4): github actions CI pipeline with build test

--- url-shortener/src/test/java/com/shortener/UrlShortenerApplicationTests.java
+++ url-shortener/src/test/java/com/shortener/UrlShortenerApplicationTests.java
@@ -2,12 +2,19 @@ package com.shortener;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:h2:mem:testdb",
+		"spring.datasource.driver-class-name=org.h2.Driver",
+		"spring.jpa.hibernate.ddl-auto=create-drop",
+		"spring.data.redis.host=localhost",
+		"spring.data.redis.port=6379"
+})
 class UrlShortenerApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
